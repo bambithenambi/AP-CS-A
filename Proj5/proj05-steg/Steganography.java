b5e2dd64c8ddca84a572e5ccbe43adf65c8be9a6
bambithenambi <abhijitvnambiar@gmail.com>
2023-03-29 10:54:49 -0700

    day3?

--- Proj5/proj05-steg/Steganography.java
+++ Proj5/proj05-steg/Steganography.java
@@ -21,10 +21,53 @@ public class Steganography {
         }
         return p;
     }
+    public static void setLow(Pixel p, Color c){
+        clearLow(p);
+        p.setRed((c.getRed())/64 + p.getRed());
+        p.setBlue((c.getBlue())/64 + p.getBlue());
+        p.setGreen((c.getGreen())/64 + p.getGreen());
+        
+
+    }
+    public static Picture testSetLow(Picture p, Color c) {
+        for (int x = 0; x < p.getWidth(); x++) {
+            // loop through all y
+            for (int y = 0; y < p.getHeight(); y++) {
+                setLow(p.getPixel(x,y), c);
+            }
+        }
+        return p;
+    }
+    public static Picture revealPicture(Picture hidden) {
+        Picture copy = new Picture(hidden);
+        Pixel[][] pixels = copy.getPixels2D();
+        Pixel[][] source = hidden.getPixels2D();
+        
+        for (int r=0; r<pixels.length; r++) {
+            for (int c=0; c<pixels[0].length; c++) {
+                Color col = source[r][c].getColor();
+            
+                //copy.getPixel(c, r).setRed((col.getRed()%4)*64);
+                //copy.getPixel(c, r).setGreen((col.getGreen()%4)*64);
+                //copy.getPixel(c, r).setBlue((col.getBlue()%4)*64);
+                //System.out.println(pixels[r][c].getRed()%64);
+                //System.out.println((col.getRed()%4)*64);
+                pixels[r][c].setRed((col.getRed()%4)*64+pixels[r][c].getRed()%64);
+                pixels[r][c].setGreen((col.getGreen()%4)*64+pixels[r][c].getGreen()%64);
+                pixels[r][c].setBlue((col.getBlue()%4)*64+pixels[r][c].getBlue()%64);
+
+            }
+        }
+        
+        return copy;
+    }
+
     public static void main(String[] args) {
-        Picture beach = new Picture ("beach.jpg");
-        beach.explore();
-        Picture copy = testClearLow(beach);
-        copy.explore();
+        Picture beach2 = new Picture ("beach.jpg");
+        beach2.explore();
+        Picture copy2 = testClearLow(beach2);
+        copy2.explore();
+        Picture copy3 = revealPicture(copy2);
+        copy3.explore();
     }
 }
